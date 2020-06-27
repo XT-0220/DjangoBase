af7dc0f55e690ecccf54966cb5830d148cae6708
J_XT <1606778306@qq.com>
2020-06-27 20:09:48 +0800

    登录

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('register/', views.RegisterView.as_view()),
 
-    path('login/', views.LoginView.as_view())
+    path('login/', views.LoginView.as_view()),
 
 
 
