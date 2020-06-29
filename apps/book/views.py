fb2446855114bec1f3b8c19ffd3da30f9b5c7295
J_XT <1606778306@qq.com>
2020-06-29 17:11:32 +0800

    数据库的增删操作

--- apps/book/views.py
+++ apps/book/views.py
@@ -1,3 +1,47 @@
+from django import http
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+from book.models import BookInfo, HeroInfo
+
+
+class AddView(View):
+    def get(self,request):
+
+        # 1.save()
+        # book = BookInfo()
+        #
+        # book.btitle = 'MANYMAN'
+        # book.bcomment = 20
+        # book.bpub_date = '2020-6-29'
+        # book.bread = 33
+        #
+        # book.save()
+
+        # 2.create() 模型类.objects.create()
+
+        # BookInfo.objects.create(
+        #     btitle = 'MANYMALE',
+        #     bcomment = 20,
+        #     bread = 55,
+        #     bpub_date = '2020-6-29'
+        #
+        # )
+
+
+        # 3.save()修改
+        # book = BookInfo.objects.get(btitle = 'MANYMALE')
+        # book.btitle = 'manymale'
+        # book.save()
+
+
+        # 4.update()  模型类.objects.filter().update()
+        # BookInfo.objects.filter(btitle = 'MANYMAN').update(btitle='manyman')
+
+
+
+
+
+        return http.HttpResponse('增删改查-增')
--- middlewares.py
+++ middlewares.py
@@ -1,7 +1,7 @@
 from django.views import View
 from django.utils.deprecation import MiddlewareMixin
 
-class TestMiddleWare1(View):
+class TestMiddleWare1(MiddlewareMixin):
 
 
     def process_request(self,request):
@@ -11,11 +11,11 @@ class TestMiddleWare1(View):
     def process_response(self,request,response):
         return response
 
-class TestMiddleWare2(View):
+class TestMiddleWare2(MiddlewareMixin):
 
     def process_request(self,request):
         print('process_request2 被调用')
-    def process_view(self,request,view_args,view_kwargs):
+    def process_view(self,request,view_func,view_args,view_kwargs):
         print('process_view2 被调用')
     def process_response(self,request,response):
         return response
